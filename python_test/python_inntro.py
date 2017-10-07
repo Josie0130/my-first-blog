cc490bb0c329d86493c263738ec6f84c77929b45
josie <jpeacock@attatec.com>
2017-10-07 13:26:20 -0500

    My Django Girls app, first commit

---
+++ python_test/python_inntro.py
@@ -0,0 +1,14 @@
+input_name = input("What is your name? ")
+
+def hi(name):
+    if name == "ola":
+        print ("Hello Ola!")
+    elif name == "fish":
+        print ("FISH!!!")
+    elif name == "Josie":
+        print ("Hi Josie")
+    else:
+        print ("I don't know you.")
+
+
+hi(input_name)
